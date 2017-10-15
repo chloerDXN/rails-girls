b84971d3db150df265939a4e6664c1c33b58a2d8
chloe.dickson <chloe.dickson@epfl.ch>
2017-10-15 10:10:16 -0400

    added picture gallery in welcome

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/info'
 
-  root to: redirect('ideas')
+  root to: redirect('pages/welcome')
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
